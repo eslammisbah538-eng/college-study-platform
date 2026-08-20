e87ad5da84b859705e05b8e5e0d5cbc203017371
Islam Misbah <eslammisbah538@gmail.com>
2026-08-21 01:48:13 +0300

    Update structureService.js

--- frontend/src/services/structureService.js
+++ frontend/src/services/structureService.js
@@ -1,20 +1,18 @@
 import api from './api';
-
 export const structureService = {
     getUniversities: () => api.get('/universities').then((res) => res.data.data),
-
     getColleges: (universityId) =>
         api.get(`/universities/${universityId}/colleges`).then((res) => res.data.data),
-
     getAcademicYears: (collegeId) =>
         api.get(`/colleges/${collegeId}/academic-years`).then((res) => res.data.data),
-
     getSemesters: (academicYearId) =>
         api.get(`/academic-years/${academicYearId}/semesters`).then((res) => res.data.data),
-
     createAcademicYear: (payload) =>
         api.post('/academic-years', payload).then((res) => res.data.data),
-
     createSemester: (payload) =>
         api.post('/semesters', payload).then((res) => res.data.data),
+    createUniversity: (payload) =>
+        api.post('/universities', payload).then((res) => res.data.data),
+    createCollege: (payload) =>
+        api.post('/colleges', payload).then((res) => res.data.data),
 };
