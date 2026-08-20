025070bdc99bd0a519cec754473c54842642efda
Islam Misbah <eslammisbah538@gmail.com>
2026-08-21 02:21:02 +0300

    Update academicStructure.service.js

--- backend/src/services/academicStructure.service.js
+++ backend/src/services/academicStructure.service.js
@@ -6,8 +6,12 @@ const structureModel = require('../models/academicStructure.model');
 
 const getAllUniversities = () => structureModel.findAllUniversities();
 
+const createUniversity = (data) => structureModel.createUniversity(data);
+
 const getCollegesByUniversity = (universityId) =>
     structureModel.findCollegesByUniversity(universityId);
+
+const createCollege = (data) => structureModel.createCollege(data);
 
 const getAcademicYearsByCollege = (collegeId) =>
     structureModel.findAcademicYearsByCollege(collegeId);
@@ -21,7 +25,9 @@ const createSemester = (data) => structureModel.createSemester(data);
 
 module.exports = {
     getAllUniversities,
+     createUniversity,
     getCollegesByUniversity,
+    createCollege,
     getAcademicYearsByCollege,
     getSemestersByAcademicYear,
     createAcademicYear,
