87bc7f0f7a0e58ffb6e3bdf44d17911420c41e57
Islam Misbah <eslammisbah538@gmail.com>
2026-08-21 03:35:07 +0300

    Refactor academic year and department API endpoints

--- frontend/src/services/structureService.js
+++ frontend/src/services/structureService.js
@@ -3,8 +3,8 @@ export const structureService = {
     getUniversities: () => api.get('/universities').then((res) => res.data.data),
     getColleges: (universityId) =>
         api.get(`/universities/${universityId}/colleges`).then((res) => res.data.data),
-    getAcademicYears: (collegeId) =>
-        api.get(`/colleges/${collegeId}/academic-years`).then((res) => res.data.data),
+      getAcademicYears: (departmentId) =>
+        api.get(`/departments/${departmentId}/academic-years`).then((res) => res.data.data),
     getSemesters: (academicYearId) =>
         api.get(`/academic-years/${academicYearId}/semesters`).then((res) => res.data.data),
     createAcademicYear: (payload) =>
@@ -15,4 +15,8 @@ export const structureService = {
         api.post('/universities', payload).then((res) => res.data.data),
     createCollege: (payload) =>
         api.post('/colleges', payload).then((res) => res.data.data),
+        getDepartments: (collegeId) =>
+        api.get(`/colleges/${collegeId}/departments`).then((res) => res.data.data),
+    createDepartment: (payload) =>
+        api.post('/departments', payload).then((res) => res.data.data),
 };
