f0889b90f2ac951d10cbda4fde2a12da4c1b4e83
Islam Misbah <eslammisbah538@gmail.com>
2026-08-21 03:57:33 +0300

    Add DepartmentPage route to AppRoutes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,26 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
-
 import MainLayout from '../layouts/MainLayout';
 import AdminLayout from '../layouts/AdminLayout';
 import ProtectedRoute from './ProtectedRoute';
 import CollegePage from '../pages/CollegePage';
 import UniversityPage from '../pages/UniversityPage';
+import DepartmentPage from '../pages/DepartmentPage';
 import Home from '../pages/Home';
-
 import AcademicYearPage from '../pages/AcademicYearPage';
 import SemesterPage from '../pages/SemesterPage';
 import SubjectPage from '../pages/SubjectPage';
 import SearchResultsPage from '../pages/SearchResultsPage';
 import UploadFilePage from '../pages/UploadFilePage';
 import NotFoundPage from '../pages/NotFoundPage';
-
 import AdminLogin from '../pages/admin/AdminLogin';
 import Dashboard from '../pages/admin/Dashboard';
 import PendingRequests from '../pages/admin/PendingRequests';
 import ManageStructure from '../pages/admin/ManageStructure';
 import ManageSubjects from '../pages/admin/ManageSubjects';
 import ManageFiles from '../pages/admin/ManageFiles';
-
 export default function AppRoutes() {
     return (
         <Routes>
@@ -28,17 +25,16 @@ export default function AppRoutes() {
             <Route element={<MainLayout />}>
                 <Route path="/" element={<Home />} />
                 <Route path="/universities/:universityId" element={<UniversityPage />} />
-                 <Route path="/colleges/:collegeId" element={<CollegePage />} />
+                <Route path="/colleges/:collegeId" element={<CollegePage />} />
+                <Route path="/departments/:departmentId" element={<DepartmentPage />} />
                 <Route path="/years/:yearId" element={<AcademicYearPage />} />
                 <Route path="/semesters/:semesterId" element={<SemesterPage />} />
                 <Route path="/subjects/:id" element={<SubjectPage />} />
                 <Route path="/search" element={<SearchResultsPage />} />
                 <Route path="/upload" element={<UploadFilePage />} />
             </Route>
-
             {/* ---------- دخول الأدمن (بدون تخطيط اللوحة) ---------- */}
             <Route path="/admin/login" element={<AdminLogin />} />
-
             {/* ---------- لوحة تحكم الأدمن (محمية) ---------- */}
             <Route
                 path="/admin"
@@ -54,7 +50,6 @@ export default function AppRoutes() {
                 <Route path="subjects" element={<ManageSubjects />} />
                 <Route path="files" element={<ManageFiles />} />
             </Route>
-
             {/* ---------- 404 ---------- */}
             <Route path="*" element={<NotFoundPage />} />
         </Routes>
