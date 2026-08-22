3eabf689e1c55817cbda2040125761f60d2d2ed1
Islam Misbah <eslammisbah538@gmail.com>
2026-08-22 20:51:58 +0300

    Update academicStructure.service.js

--- backend/src/services/academicStructure.service.js
+++ backend/src/services/academicStructure.service.js
@@ -8,13 +8,20 @@ const getAllUniversities = () => structureModel.findAllUniversities();
 
 const createUniversity = (data) => structureModel.createUniversity(data);
 
+const deleteUniversity = (id) => structureModel.deleteUniversity(id);
+
 const getCollegesByUniversity = (universityId) =>
     structureModel.findCollegesByUniversity(universityId);
 
 const createCollege = (data) => structureModel.createCollege(data);
 
+const deleteCollege = (id) => structureModel.deleteCollege(id);
+
 const getDepartmentsByCollege = (collegeId) => structureModel.findDepartmentsByCollege(collegeId);
+
 const createDepartment = (data) => structureModel.createDepartment(data);
+
+const deleteDepartment = (id) => structureModel.deleteDepartment(id);
 
 const getAcademicYearsByDepartment = (departmentId) =>
     structureModel.findAcademicYearsByDepartment(departmentId);
@@ -24,17 +31,26 @@ const getSemestersByAcademicYear = (academicYearId) =>
 
 const createAcademicYear = (data) => structureModel.createAcademicYear(data);
 
+const deleteAcademicYear = (id) => structureModel.deleteAcademicYear(id);
+
 const createSemester = (data) => structureModel.createSemester(data);
+
+const deleteSemester = (id) => structureModel.deleteSemester(id);
 
 module.exports = {
     getAllUniversities,
     createUniversity,
+    deleteUniversity,
     getCollegesByUniversity,
     createCollege,
+    deleteCollege,
     getDepartmentsByCollege,
     createDepartment,
+    deleteDepartment,
     getAcademicYearsByDepartment,
     createAcademicYear,
+    deleteAcademicYear,
     getSemestersByAcademicYear,
     createSemester,
+    deleteSemester,
 };
