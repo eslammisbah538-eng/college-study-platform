2b841c95e11eee62c0a2e0a47024b9b85c8f4d47
Islam Misbah <eslammisbah538@gmail.com>
2026-08-22 20:58:22 +0300

    Add delete methods for universities, colleges, and more

--- frontend/src/services/structureService.js
+++ frontend/src/services/structureService.js
@@ -1,22 +1,32 @@
 import api from './api';
+
 export const structureService = {
     getUniversities: () => api.get('/universities').then((res) => res.data.data),
-    getColleges: (universityId) =>
-        api.get(`/universities/${universityId}/colleges`).then((res) => res.data.data),
-      getAcademicYears: (departmentId) =>
-        api.get(`/departments/${departmentId}/academic-years`).then((res) => res.data.data),
-    getSemesters: (academicYearId) =>
-        api.get(`/academic-years/${academicYearId}/semesters`).then((res) => res.data.data),
-    createAcademicYear: (payload) =>
-        api.post('/academic-years', payload).then((res) => res.data.data),
-    createSemester: (payload) =>
-        api.post('/semesters', payload).then((res) => res.data.data),
     createUniversity: (payload) =>
         api.post('/universities', payload).then((res) => res.data.data),
+    deleteUniversity: (id) => api.delete(`/universities/${id}`).then((res) => res.data.data),
+
+    getColleges: (universityId) =>
+        api.get(`/universities/${universityId}/colleges`).then((res) => res.data.data),
     createCollege: (payload) =>
         api.post('/colleges', payload).then((res) => res.data.data),
-        getDepartments: (collegeId) =>
+    deleteCollege: (id) => api.delete(`/colleges/${id}`).then((res) => res.data.data),
+
+    getDepartments: (collegeId) =>
         api.get(`/colleges/${collegeId}/departments`).then((res) => res.data.data),
     createDepartment: (payload) =>
         api.post('/departments', payload).then((res) => res.data.data),
+    deleteDepartment: (id) => api.delete(`/departments/${id}`).then((res) => res.data.data),
+
+    getAcademicYears: (departmentId) =>
+        api.get(`/departments/${departmentId}/academic-years`).then((res) => res.data.data),
+    createAcademicYear: (payload) =>
+        api.post('/academic-years', payload).then((res) => res.data.data),
+    deleteAcademicYear: (id) => api.delete(`/academic-years/${id}`).then((res) => res.data.data),
+
+    getSemesters: (academicYearId) =>
+        api.get(`/academic-years/${academicYearId}/semesters`).then((res) => res.data.data),
+    createSemester: (payload) =>
+        api.post('/semesters', payload).then((res) => res.data.data),
+    deleteSemester: (id) => api.delete(`/semesters/${id}`).then((res) => res.data.data),
 };
